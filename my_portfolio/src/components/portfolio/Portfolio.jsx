5f289988e93780ad08d9c8df1a39081c3916de0a
it21038914 <it21038914@my.sliit.lk>
2023-06-18 23:38:42 +0530

    s

--- my_portfolio/src/components/portfolio/Portfolio.jsx
+++ my_portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,80 @@
-import React from "react";
 import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Garment Manement System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Trafic Police System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Phone Shop System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Education Mangement System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Garment Manement System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Garment Manement System",
+    github: "https://github.com",
+    demo: "https://google.com",
+  },
+];
 function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        {data.map((item) => (
+          <article key={item.id} className="portfolio_item">
+            <div className="portfolio_item-image">
+              <img src={item.image} alt="" />
+            </div>
+            <h3>{item.title}</h3>
+            <div className="portfolio_item-cta">
+              <a href={item.github} className="btn" target="_blank">
+                Github
+              </a>
+              <a href={item.demo} className="btn btn-primary" target="_blank">
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
--- my_portfolio/src/components/testimonials/Testimonials.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Testimonials.css";
-
-function Terminials() {
-  return <section id="testimonials">Terminials</section>;
-}
-
-export default Terminials;
