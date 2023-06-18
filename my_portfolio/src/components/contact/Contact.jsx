7fd9a3867fbac8ec5c726d00c9fc4e5abb5f41cb
it21038914 <it21038914@my.sliit.lk>
2023-06-19 02:03:21 +0530

    almost finished

--- my_portfolio/src/components/contact/Contact.jsx
+++ my_portfolio/src/components/contact/Contact.jsx
@@ -1,11 +1,91 @@
-import React from "react";
 import "./Contact.css";
-
+import { AiOutlineMail } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_vfdzels",
+        "template_20fik5o",
+        form.current,
+        "xnRnI4IXJoNMFWW2p"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <AiOutlineMail className="contact_option_icon" />
+            <h4>Email</h4>
+            <h5>kavindugihan222@gmail.com</h5>
+            <a href="mailto:kavindugihan222@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsLinkedin className="contact_option_icon" />
+            <h4>Linkedin</h4>
+            <h5>Kavidu Gihan</h5>
+            <a
+              href="https://www.linkedin.com/in/kavidu-gihan-28519b26b/"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option_icon" />
+            <h4>Whatsapp</h4>
+            <h5>0703889630</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=94703889630"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Fukk Name"
+            required
+          />
+          <input type="email" name="email" placeholder="your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
--- my_portfolio/src/components/footer/Footer.jsx
+++ my_portfolio/src/components/footer/Footer.jsx
@@ -1,7 +1,51 @@
-import React from "react";
 import "./Footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 function Footer() {
-  return <div>Footer123</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Kavindu Gihan
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Projects</a>
+        </li>
+
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href="">
+          <FaFacebookF />
+        </a>
+
+        <a href="">
+          <BsLinkedin />
+        </a>
+        <a href="">
+          <BsTwitter />
+        </a>
+      </div>{" "}
+      <div className="footer_copyright">
+        <small>&copy; All rights reserved</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
